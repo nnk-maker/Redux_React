60f02327401d10d248a1a3a96552b41491d6e1ee
nnk-maker <krishnanarasimha4@gmail.com>
2022-04-19 19:42:07 +0200

    react redux
    react redux

---
+++ reactredux/src/reducer.js
@@ -0,0 +1,20 @@
+//import React from 'react';
+
+const count = 5;
+export default function reducer(state = count,action) {
+    const {type,payload} = action;
+  
+    switch(type) {
+        case "INCREMENT":
+            return state+payload;
+
+        case "DECREMENT":
+            return state-1
+        default:
+            return state;
+
+    }
+  
+  
+    
+}
